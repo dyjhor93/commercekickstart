7f51f2c98f76c0c5c2f23fcad1e1072305899ac1
Desarrollo1 <sistemas1@cervial.com>
2020-09-25 21:01:04 -0500

    Crear subtemas cetiia 3-10

---
+++ sites/all/themes/cetiia_09/script_09.js
@@ -0,0 +1,15 @@
+jQuery(document).ready(function($) {
+    // Codigo jquery y javascript va aquí
+    //alert();
+    //$("#main-menu").hide();//ocultar
+    //$("#main-menu").removeClass();//eliminar todas las clases
+    //$(".navigation").removeClass("clearfix");//eliminar una clase especifica
+    //$("#main-menu").addClass("custom-menu");//agregar una clase
+    //var classList = document.getElementById('main-menu').className.split(/\s+/);
+    //for (var i = 0; i < classList.length; i++) {
+        // if (classList[i] === 'someClass') {
+        //     //do something
+        // }
+        //console.log(classList[i]);//escribe las clases en el inspector de codigo
+    //}
+  });
